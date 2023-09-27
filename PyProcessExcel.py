14a39cb97e967a6c91cff8f966ff5e8449964d6a
Nguyen Hoang <nguye@HNASUS.mylab.local>
2023-09-26 23:19:53 -0500

    Initialize project

---
+++ PyProcessExcel.py
@@ -0,0 +1,9 @@
+import pandas as pd
+print('Hello World')
+excelPath = 'D:\Chirality Files\DEV\Python\PyProcessExcelFile\Book1.xlsx' 
+print('excelPath=['+ excelPath + ']') 
+df = pd.read_excel(excelPath)
+print(df)
+
+
+
