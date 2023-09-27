023b445b8f9806bcede58f156774970538c98947
nick-nguyen-bis <nicknguyen.bis@gmail.com>
2023-09-26 23:28:19 -0500

    Add a new note

--- PyProcessExcel.py
+++ PyProcessExcel.py
@@ -4,6 +4,7 @@ excelPath = 'D:\Chirality Files\DEV\Python\PyProcessExcelFile\Book1.xlsx'
 print('excelPath=['+ excelPath + ']') 
 df = pd.read_excel(excelPath)
 print(df)
+print('Program ends here !')
 
 
 
